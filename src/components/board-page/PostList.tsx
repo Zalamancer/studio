14742e1487d933e04ebeac5c1089388c587a39be
Ihsan Duru <duruihsan@gmail.com>
2025-06-21 05:28:58 +0000

    Make the following changes:
    On the page src/app/page.tsx,
    
    Find this el

--- src/components/board-page/PostList.tsx
+++ src/components/board-page/PostList.tsx
@@ -36,7 +36,7 @@ export const PostList: React.FC<PostListProps> = ({
   return (
     <div className="flex flex-col h-full relative">
       <ScrollArea className="flex-grow overflow-y-auto min-h-0">
-        <div className="md:columns-2 gap-4 space-y-4">
+        <div className="md:columns-2 gap-4 space-y-4 md:p-0">
           {posts.length > 0 ? (
             posts.map((post, index) => (
               <PostCard key={post.id} post={post} onOpen={onPostSelect} isSelected={selectedPostId === post.id} isPriority={index < 2}/>
